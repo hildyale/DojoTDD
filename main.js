b62058bfc21fdef0e26e2b017b9cc2ff0987e49a
alejandro.isazad <hildyale@gmail.com>
2018-05-12 11:53:33 -0500

    primer build

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,function(){
-    console.log("Servidor corriendo");
+    console.log("Servidor corriendo bien");
     
 });
 
--- test.js
+++ test.js
@@ -1,30 +1,6 @@
 const codebreaker = require('./codebreaker');
 var assert = require('chai').assert;
 
-//var romanize= require('./romanize');
-
-/*
-describe('Pruebaromanize', function(){
-    var testsverify=[
-        {args: 1, expected: "I"},
-        {args: 5, expected: "V"},
-        {args: 10, expected: "X"},
-        {args: 20, expected: "XX"},
-        {args: 50, expected: "L"},
-        {args: 100, expected: "C"},
-        {args: 3, expected: "III"},
-        {args: 58, expected: "LVII"},
-        {args: 60, expected: "LX"}
-    ];
-
-    testsverify.forEach(function (test) { 
-        it(' Convertidos de manera correcta ' + test.args , function () 
-        { var res = romanize.romanize(test.args);
-             assert.equal(res, test.expected); 
-        });
-    });
-});
-*/
 let instanceCodeBreaker = new codebreaker("1234");
 
 describe('CODEBREAKER', function(){
