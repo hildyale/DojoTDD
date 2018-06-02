3fabeb188bf5ed52a692eea6d6ff89f504403477
sonorkz <santiago.ramiress@gmail.com>
2018-06-02 14:31:04 -0500

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var assert = require('chai').assert;
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
 
@@ -8,6 +9,7 @@ const api=require('./routes');
 // Importamos el facilitador de peticiones 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api',api);// para que todas las rutas usen el prefijo api/
 
 
